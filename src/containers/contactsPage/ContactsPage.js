7b20acf09ed74be2835a7bc52270d665d7be66ca
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-01-22 19:10:24 +0100

    Finish ContactsPage.js container

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = (props) => {
+  const { contacts, addContact } = props;
+
+  // Define state variables for contact info and duplicate check
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    // Add contact info and clear data if the contact name is not a duplicate
+    if (!duplicate) {
+      addContact(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  // Using hooks, check for contact name in the contacts array variable in props
+  useEffect(() => {
+    if (contacts.find((contact) => contact.name === name)) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm   
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
