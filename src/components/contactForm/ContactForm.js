eb696560b2cbff149b07505226fcb2584affa046
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-01-24 18:35:06 +0100

    Add ContactForm.js return value as a form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,27 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="tel"
+        placeholder="Phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="^(?:(?:\\+|00)47)?[13-9]\\d{7}$"
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input type="submit" value="Add Contact" />
+    </form>
   );
 };
