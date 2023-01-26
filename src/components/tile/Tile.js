4823cb797761f98369fc6194b79b1499e693106c
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-01-26 20:10:47 +0100

    Add tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
   return (
     <div className="tile-container">
-      
+      {props.contact.map((value, index) => {
+        if (index === 0) {
+          return <p className="tile-tile" key={index}>{value}</p>;
+        } else {
+          return <p className="tile" key={index}>{value}</p>;
+        }
+      }
+      )}
     </div>
   );
 };
